926c9f256e9a53ed815a1bab0bd4240b073b0b00
James Chua <james@leadiq.com>
2023-01-31 20:30:32 +0800

    fix issues with batch decoding

--- conditionme/rollout/rollout_model.py
+++ conditionme/rollout/rollout_model.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
-from transformers import GenerationMixin, PreTrainedTokenizer, PreTrainedTokenizerBase
+import torch
+from transformers import PreTrainedTokenizerBase, BatchEncoding
 
 from conditionme.modified_gpt2_lm_head import ModifiedGPT2LMHeadModel
 
@@ -11,18 +12,16 @@ def complete_text_with_reward(
     tokenizer: PreTrainedTokenizerBase,
     model: ModifiedGPT2LMHeadModel,
     temperature: float = 1.0,
-    # max int
-    max_length: int = 9999999999,
 ) -> str:
     device = model.device
-    query_tensor = tokenizer.encode(prompt, return_tensors="pt").to(device)  # type: ignore
+    prompt_with_bos = f"{tokenizer.bos_token}{prompt}"
+    query_tensor = tokenizer.encode(prompt_with_bos, return_tensors="pt").to(device)  # type: ignore
     generation_output = model.generate(
         query_tensor,
         target_reward=target_reward,
         return_dict_in_generate=True,
         output_scores=True,
         temperature=temperature,
-        max_length=max_length,
     )
     # generated sequence
     generated_sequence = generation_output["sequences"]
@@ -33,26 +32,28 @@ def complete_text_with_reward(
 
 
 def complete_text_with_reward_batched(
-    prompt: List[str],
+    prompt_with_bos: List[str],
     target_reward: List[float],
     tokenizer: PreTrainedTokenizerBase,
     model: ModifiedGPT2LMHeadModel,
     temperature: float = 1.0,
-    max_length: Optional[int] = None,
 ) -> List[str]:
     device = model.device
-    query_tensor = tokenizer.batch_encode_plus(prompt, return_tensors="pt").to(device)  # type: ignore
+    # for each prompt, add the bos token
+    prompt_with_bos = [f"{tokenizer.bos_token}{p}" for p in prompt_with_bos]
+    prompt_encoding: BatchEncoding = tokenizer.batch_encode_plus(prompt_with_bos, return_tensors="pt", padding=True, return_attention_mask=True).to(device)  # type: ignore
+    input_ids: torch.Tensor = prompt_encoding["input_ids"]
+    attention_mask: torch.Tensor = prompt_encoding["attention_mask"]
     generation_output = model.generate(
-        query_tensor,
+        input_ids=input_ids,
+        attention_mask=attention_mask,
         target_reward=target_reward,
         return_dict_in_generate=True,
         output_scores=True,
         temperature=temperature,
-        max_length=max_length,
     )
     # generated sequence
     generated_sequence = generation_output["sequences"]
-    flattened_sequence = generated_sequence.flatten()
     # convert to text
-    generated_text: List[str] = tokenizer.batch_decode(flattened_sequence)
+    generated_text: List[str] = tokenizer.batch_decode(generated_sequence)
     return generated_text
