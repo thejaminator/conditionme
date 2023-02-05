7480ebc19a088a028dc93c750478bdddda33f44c
James Chua <james@leadiq.com>
2023-02-05 18:08:49 +0800

    fix saving normalizer

--- tests/test_save_normalizer.py
+++ tests/test_save_normalizer.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
-from conditionme.normalization.normalizer import DoNothingNormalizer, RewardNormalizer
+from conditionme.normalization.normalizer import (
+    DoNothingNormalizer,
+    RewardNormalizer,
+    StandardScaleNormalizer,
+)
 
 
 def test_save_normalizer():
@@ -8,3 +12,10 @@ def test_save_normalizer():
     DoNothingNormalizer().save_normalizer(path)
     normalizer = RewardNormalizer.load_normalizer(path)
     assert isinstance(normalizer, DoNothingNormalizer)
+
+
+def test_save_standard_normalizer():
+    path = Path("test_normalizer")
+    StandardScaleNormalizer(mean=1, std=1).save_normalizer(path)
+    normalizer = RewardNormalizer.load_normalizer(path)
+    assert isinstance(normalizer, StandardScaleNormalizer)
