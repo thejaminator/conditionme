fix attention mask

conditionme/cond_gpt2_tokenize.py
@@ -8,6 +8,7 @@ from transformers import (
     TensorType,
     GPT2Tokenizer,
 )
+from transformers.utils import PaddingStrategy
 
 from settings import DEFAULT_REWARD_TOKEN_ID
 
@@ -68,8 +69,8 @@ def batch_tokenize_gpt2(
                 tokenized_ids_with_special_tokens, max_length=tokenizer.model_max_length
             )
         },
-        max_length=tokenizer.model_max_length,
-        return_attention_mask=True
+        padding=PaddingStrategy.LONGEST,
+        return_attention_mask=True,
     )
     inputs_ids = tokenizer_result["input_ids"]
     for i, input_id_row in enumerate(inputs_ids):
@@ -87,4 +88,3 @@ def batch_tokenize_gpt2(
     for key in tokenizer_result:
         new_dict[key] = torch.tensor(tokenizer_result[key])
     return new_dict
-

conditionme/modified_gpt2_forward.py
@@ -75,12 +75,15 @@ def modfied_transformer_forward(
         past_key_values = tuple([None] * len(transformer_model.h))
     else:
         past_length = past_key_values[0][0].size(-2)
-    # if position_ids is None:
 
+    # Using the provided attention mask, we create position_ids
     if attention_mask is not None and position_ids is None:
         # create position_ids on the fly for batch generation
         position_ids = attention_mask.long().cumsum(-1) - 1
         position_ids.masked_fill_(attention_mask == 0, 1)
+        position_ids = (
+            position_ids[:, -1].unsqueeze(-1) if past_length else position_ids
+        )
     else:
         position_ids = torch.arange(
             past_length,